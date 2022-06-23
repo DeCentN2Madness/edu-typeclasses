2a721fdb705bc923603934f2f5ad5c5224f25272
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-06-23 13:50:44 -0700

    setup folding lesson

---
+++ main.hs
@@ -0,0 +1,21 @@
+import Data.Char
+
+rejectNonalphabetic :: String -> Maybe String
+rejectNonalphabetic string =
+    case myAll isAlpha string of
+        False -> Nothing
+        True  -> Just string
+
+myAll :: (a -> Bool) -> [a] -> Bool
+myAll _ [] = True
+myAll pred (x:xs) =
+    case pred x of
+        False -> False
+        True -> myAll pred xs
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny _ [] = False
+myAny pred (x:xs) =
+    case pred x of
+        True -> True
+        False -> myAny pred xs
