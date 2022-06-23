0cbec73e0eee13611e935b464cce79814431936d
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-06-23 14:08:03 -0700

    add (extracurricular) myAlpha function

--- main.hs
+++ main.hs
@@ -2,9 +2,12 @@ import Data.Char
 
 rejectNonalphabetic :: String -> Maybe String
 rejectNonalphabetic string =
-    case myAll isAlpha string of
+    case myAll myAlpha string of
         False -> Nothing
         True  -> Just string
 
 myAll :: (a -> Bool) -> [a] -> Bool
 myAll pred = foldr (\ x y -> pred x && y) True
+
+myAlpha :: Char -> Bool
+myAlpha c = elem c ['a'..'z'] || elem c ['A'..'Z']
