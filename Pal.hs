277c81f984d65e47d1ff836a448ae4a0c0e94850
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-06-23 11:55:02 -0700

    add isPalindrome pipeline to Pal module

--- Pal.hs
+++ Pal.hs
@@ -3,7 +3,31 @@ module Pal where
 import Data.Char  ( toLower, isPunctuation )
 
 isPalindrome :: String -> Maybe Bool
-isPalindrome = undefined
+isPalindrome string = isOwnReverseMaybe $ rejectEmpty $ normalize string
+
+normalize :: String -> String
+normalize string = filter notSpace $ allLowerCase $ filter notPunctuation string
+
+notSpace :: Char -> Bool
+notSpace = (/= ' ')
+
+allLowerCase :: String -> String
+allLowerCase = map toLower
+
+notPunctuation :: Char -> Bool
+notPunctuation char = not $ isPunctuation char
+
+rejectEmpty :: String -> Maybe String
+rejectEmpty word =
+    case word of
+        "" -> Nothing
+        _  -> Just word
+
+isOwnReverseMaybe :: Maybe String -> Maybe Bool
+isOwnReverseMaybe maybeString =
+    case maybeString of
+        Nothing -> Nothing
+        Just string -> Just $ isOwnReverse string
 
 isOwnReverse :: String -> Bool
 isOwnReverse word =
@@ -12,21 +36,6 @@ isOwnReverse word =
 isPalindromePhrase :: String -> Bool
 isPalindromePhrase phrase =
     isOwnReverse $ filter notSpace phrase
-
-notSpace :: Char -> Bool
-notSpace = (/= ' ')
-
-notPunctuation :: Char -> Bool
-notPunctuation char = not $ isPunctuation char
-
-nonemptyPal :: String -> Maybe Bool
-nonemptyPal word =
-    case word of
-        "" -> Nothing
-        _  -> Just $ isOwnReverse word
-
-allLowerCase :: String -> String
-allLowerCase = map toLower
 
 isPalindromeIgnoringCase :: String -> Bool
 isPalindromeIgnoringCase word =
