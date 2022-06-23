e89a83bcdb03db6420a34b85081d062bc1b114e6
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-06-23 13:58:46 -0700

    remove unused cruft

--- main.hs
+++ main.hs
@@ -8,16 +8,3 @@ rejectNonalphabetic string =
 
 myAll :: (a -> Bool) -> [a] -> Bool
 myAll pred = foldr (\ x y -> pred x && y) True
-
-{- myAll _ [] = True
-myAll pred (x:xs) =
-    case pred x of
-        False -> False
-        True -> myAll pred xs -}
-
-myAny :: (a -> Bool) -> [a] -> Bool
-myAny _ [] = False
-myAny pred (x:xs) =
-    case pred x of
-        True -> True
-        False -> myAny pred xs
