eab9545cd4784a930a31dc65067654cd79c95d8c
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-06-25 13:29:15 -0700

    move main function to top
    (extracurricular, personal preference)

--- validate/src/Main.hs
+++ validate/src/Main.hs
@@ -1,14 +1,14 @@
 module Main where
 
+main :: IO ()
+main = do
+  putStrLn "Please enter a password"
+  password <- getLine
+  print (maxLength password)
+
 maxLength :: String -> Maybe String
 maxLength "" = Nothing
 maxLength xs =
   case (length xs > 20) of
     True -> Nothing
     False -> Just xs
-
-main :: IO ()
-main = do
-  putStrLn "Please enter a password"
-  password <- getLine
-  print (maxLength password)
