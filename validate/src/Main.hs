7cd527456d83af32eb311ed8688ac7accb77a017
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-06-25 15:25:27 -0700

    add allAlpha function

--- validate/src/Main.hs
+++ validate/src/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
+import Data.Char (isAlphaNum)
+
 main :: IO ()
 main = do
   putStrLn "Please enter a password"
   password <- getLine
-  print (maxLength password)
+  print (allAlpha password)
 
 maxLength :: String -> Maybe String
 maxLength "" = Nothing
@@ -12,3 +14,10 @@ maxLength xs =
   case (length xs > 20) of
     True -> Nothing
     False -> Just xs
+
+allAlpha :: String -> Maybe String
+allAlpha "" = Nothing
+allAlpha xs =
+  case all isAlphaNum xs of
+    False -> Nothing
+    True  -> Just xs
