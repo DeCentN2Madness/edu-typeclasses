61f660173680a8dd5b54905a116088a729d04ee4
DeCentN2Madness <7983+DeCentN2Madness@users.noreply.github.com>
2022-06-25 13:24:58 -0700

    add maxLength function

--- validate/src/Main.hs
+++ validate/src/Main.hs
@@ -1,5 +1,14 @@
 module Main where
 
+maxLength :: String -> Maybe String
+maxLength "" = Nothing
+maxLength xs =
+  case (length xs > 20) of
+    True -> Nothing
+    False -> Just xs
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Please enter a password"
+  password <- getLine
+  print (maxLength password)
